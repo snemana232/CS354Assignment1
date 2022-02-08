36d0a0419a51417f68cc53fc329be67123afd5bd
Srikanth Nemana <snemana@cs.utexas.edu>
2022-02-08 14:19:32 -0600

    not started yet

---
+++ ray/build/src/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/u/snemana/CS354/proj1/ray/src/RayTracer.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/RayTracer.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/SceneObjects/Box.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Box.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/SceneObjects/Cone.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/SceneObjects/Cylinder.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/SceneObjects/Sphere.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/SceneObjects/Square.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Square.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/SceneObjects/trimesh.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/fileio/bitmap.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/fileio/bitmap.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/fileio/buffer.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/fileio/buffer.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/fileio/images.cc" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/fileio/images.cc.o"
+  "/u/snemana/CS354/proj1/ray/src/fileio/pngimage.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/fileio/pngimage.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/main.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/main.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/parser/Parser.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/parser/Parser.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/parser/ParserException.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/parser/ParserException.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/parser/Token.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/parser/Token.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/parser/Tokenizer.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/parser/Tokenizer.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/bbox.cc" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/bbox.cc.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/camera.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/camera.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/cubeMap.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/cubeMap.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/light.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/light.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/material.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/material.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/ray.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/ray.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/scene/scene.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/scene/scene.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/CommandLineUI.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/CubeMapChooser.cxx" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/GraphicalUI.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/ModelerCamera.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/TraceGLWindow.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/TraceUI.cc" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/TraceUI.cc.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/debuggingView.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/debuggingView.cpp.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/debuggingWindow.cxx" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o"
+  "/u/snemana/CS354/proj1/ray/src/ui/glObjects.cpp" "/u/snemana/CS354/proj1/ray/build/src/CMakeFiles/ray.dir/ui/glObjects.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLM_ENABLE_EXPERIMENTAL"
+  "GLM_FORCE_RADIANS=1"
+  "GLM_FORCE_SIZE_FUNC=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/opt/local/include"
+  "../lib"
+  "/usr/lib/fltk"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
